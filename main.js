e93afe6bfcb30fe2f57aaa3af42007f6632527b0
GabyAguiar <gabi.freiman@hotmail.com>
2022-06-20 21:47:33 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const conhecaDestino = () => {
+    const destino = document.getElementById('destino').value;
+    const url = 
+    fetch(url).then(console.log);
+
+
+}
+
+
+document.getElementById('destino')
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+ 
